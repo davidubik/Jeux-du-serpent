50094f279e0d95cf5fb0bad21b28e00dd79d386c
davidubik <dtwebdev@outlook.fr>
2020-06-12 15:08:10 +0200

    Dans la fonction init ajout de la variable ctx canvas et la fonction refreshCanvas. Dans la fonction refreshCanvas : ajout des variables x et y Coords qui sont déclaré hors du scope initialiser à 0, mais dans le scope elles prennent += 2 à chaque appel de la fonction, ajout de la fonction clearRect sur le ctx sur la w et h du canvas, pour qu'il se redessine à chaque fois. Ajout de la fonction fillRect sur le ctx avec comme paramètre xCoord et yCoord, ajout de la fonction setTimeout avec refreshCanvas et delay comme paramètre.)

--- js/script.js
+++ js/script.js
@@ -2,7 +2,9 @@ window.onload = function()
 {
   let canvas;
   let ctx;
-  let delay = 1000;
+  let delay = 100;
+  let xCoord = 0;
+  let yCoord = 0;
 
   function init()
   {
@@ -11,17 +13,22 @@ window.onload = function()
     canvas.height = 600;
     canvas.style.border = "1px solid grey";
     document.body.appendChild(canvas);
-  }
-
-  function refreshCanvas()
-  {
     ctx = canvas.getContext('2d');
-    ctx.fillStyle = "red";
-    ctx.fillRect(30, 30, 100, 50);
+    refreshCanvas();
   }
 
-    init();
-    refreshCanvas();
+    function refreshCanvas()
+    {
+      xCoord += 2;
+      yCoord += 2;
+      ctx.clearRect(0, 0, canvas.width, canvas.height)
+      ctx.fillStyle = "red";
+      ctx.fillRect(xCoord,yCoord, 100, 50);
+      setTimeout(refreshCanvas,delay);
+    }
+
+        init();
+
 
 
 
