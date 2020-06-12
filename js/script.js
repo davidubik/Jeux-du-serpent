aebf3f656c882d2c0a4397bba33617462d1668ee
davidubik <dtwebdev@outlook.fr>
2020-06-12 12:57:19 +0200

    création des fonctions init et refreshCanvas, et décalration des variables canvas, ctx, delay hors du scope des fonctions.

--- js/script.js
+++ js/script.js
@@ -1,14 +1,28 @@
 window.onload = function()
 {
-  let canvas = document.createElement('canvas');
-  canvas.width = 900;
-  canvas.height = 600;
-  canvas.style.border = "1px solid grey";
-  document.body.appendChild(canvas);
+  let canvas;
+  let ctx;
+  let delay = 1000;
 
-  let ctx = canvas.getContext('2d');
-  ctx.fillStyle = "red";
-  ctx.fillRect(30, 30, 100, 50);
+  function init()
+  {
+    canvas = document.createElement('canvas');
+    canvas.width = 900;
+    canvas.height = 600;
+    canvas.style.border = "1px solid grey";
+    document.body.appendChild(canvas);
+  }
+
+  function refreshCanvas()
+  {
+    ctx = canvas.getContext('2d');
+    ctx.fillStyle = "red";
+    ctx.fillRect(30, 30, 100, 50);
+  }
+
+    init();
+    refreshCanvas();
+
 
 
 }
