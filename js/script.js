05ff8ea48ee68a0ed33385e9e69cf812a18ff3e7
davidubik <dtwebdev@outlook.fr>
2020-06-28 21:59:32 +0200

    Changement du mot cles var pour const et let sur toute la page.

--- js/script.js
+++ js/script.js
@@ -1,14 +1,15 @@
 window.onload = function()
 {
-  let canvasWidth = 900;  //largeur du canvas de contour
-  let canvasHeight = 600; // hauteur du canvas de contour
-  let blockSize = 30; // taille des blocks du serpent
-  let ctx;
+  const canvasWidth = 900;  //largeur du canvas de contour
+  const canvasHeight = 600; // hauteur du canvas de contour
+  const blockSize = 30; // taille des blocks du serpent
+  const widthInBlocks = canvasWidth/blockSize;
+  const heightInBlocks = canvasHeight/blockSize;
+  const canvas = document.createElement('canvas');
+  const ctx = canvas.getContext('2d');
   let delay = 100;
   let snakee;
   let applee;
-  let widthInBlocks = canvasWidth/blockSize;
-  let heightInBlocks = canvasHeight/blockSize;
   let score;
   let timeout;
 
@@ -16,7 +17,7 @@ window.onload = function()
 
   function init()
   {
-    let canvas = document.createElement('canvas');
+    // const canvas = document.createElement('canvas');
     canvas.width = canvasWidth;
     canvas.height = canvasHeight;
     canvas.style.border = "30px solid grey";
@@ -24,7 +25,7 @@ window.onload = function()
     canvas.style.display = "block";
     canvas.style.backgroundColor = "#ddd";
     document.body.appendChild(canvas);
-    ctx = canvas.getContext('2d');
+    // ctx = canvas.getContext('2d');
     snakee = new Snake([[6,4], [5,4], [4,4], [3,4], [2,4]], "right");
     applee = new Apple([10, 10]);
     score = 0;
@@ -66,8 +67,8 @@ window.onload = function()
       ctx.textBaseline = "middle";
       ctx.strokeStyle = "#fff";
       ctx.lineWidth = 3; // petit beug sur le contour du M à partir de 4px
-      let centerX = canvasWidth / 2;
-      let centerY = canvasHeight / 2;
+      const centerX = canvasWidth / 2;
+      const centerY = canvasHeight / 2;
       ctx.strokeText("GAME OVER", centerX, centerY - 180);
       ctx.fillText("GAME OVER", centerX, centerY - 180);
       ctx.font = "bold 30px sans-serif";
@@ -90,15 +91,15 @@ window.onload = function()
       ctx.fillStyle = "gray";
       ctx.textAlign = "center";
       ctx.textBaseline = "middle";
-      let centerX = canvasWidth / 2;
-      let centerY = canvasHeight / 2;
+      const centerX = canvasWidth / 2;
+      const centerY = canvasHeight / 2;
       ctx.fillText(score.toString(), centerX, centerY);
       ctx.restore();
     }
 
     function drawBlock(ctx, position){
-      let x = position[0] * blockSize;
-      let y = position[1] * blockSize;
+      const x = position[0] * blockSize;
+      const y = position[1] * blockSize;
       ctx.fillRect(x, y, blockSize, blockSize)
     }
 
@@ -115,7 +116,7 @@ window.onload = function()
         ctx.restore();
       };
       this.advence = function(){  // Method pour l'animation du serpent
-        let nextPosition = this.body[0].slice();
+        const nextPosition = this.body[0].slice();
         switch (this.direction) {
           case "left":
           nextPosition[0] -= 1;
@@ -161,16 +162,16 @@ window.onload = function()
 
         let wallCollision = false;
         let snakeCollision = false;
-        let head = this.body[0];
-        let rest = this.body.slice(1);
-        let snakeX = head[0];
-        let snakeY = head[1];
-        let minX = 0;
-        let minY = 0;
-        let maxX = widthInBlocks -1;
-        let maxY = heightInBlocks -1;
-        let isNotBetweenHorizontalWalls = snakeX < minX || snakeX >  maxX;
-        let isNotBetweenVerticalWalls = snakeY < minY || snakeY >  maxY;
+        const head = this.body[0];
+        const rest = this.body.slice(1);
+        const snakeX = head[0];
+        const snakeY = head[1];
+        const minX = 0;
+        const minY = 0;
+        const maxX = widthInBlocks -1;
+        const maxY = heightInBlocks -1;
+        const isNotBetweenHorizontalWalls = snakeX < minX || snakeX >  maxX;
+        const isNotBetweenVerticalWalls = snakeY < minY || snakeY >  maxY;
 
         if (isNotBetweenHorizontalWalls || isNotBetweenVerticalWalls) {
           wallCollision = true;
@@ -182,10 +183,10 @@ window.onload = function()
         }
         return wallCollision || snakeCollision;
       };
-      this.isEatingApple = (appleToEat) =>
+      this.isEatingApple = (appconstoEat) =>
       {
-        let head = this.body[0];
-        if (head[0] === appleToEat.position[0] && head[1] === appleToEat.position[1]) {
+        const head = this.body[0];
+        if (head[0] === appconstoEat.position[0] && head[1] === appconstoEat.position[1]) {
           return true;
         }
         else {
@@ -200,21 +201,21 @@ window.onload = function()
         ctx.save();
         ctx.fillStyle = "chartreuse";
         ctx.beginPath();
-        let radius = blockSize / 2;
-        let x = this.position[0] * blockSize + radius;
-        let y = this.position[1] * blockSize + radius;
+        const radius = blockSize / 2;
+        const x = this.position[0] * blockSize + radius;
+        const y = this.position[1] * blockSize + radius;
         ctx.arc(x, y, radius, 0, Math.PI*2, true);
         ctx.fill();
         ctx.restore();
       };
       this.setNewPosition = () =>{ // METHOD POUR QUE LA POMME CHANGE DE POSITION QUAND ELLE À ÉTÉ MANGER PAR LER SERPENT
-        let newX = Math.round(Math.random() * (widthInBlocks -1));
-        let newY = Math.round(Math.random() * (heightInBlocks -1));
+        const newX = Math.round(Math.random() * (widthInBlocks -1));
+        const newY = Math.round(Math.random() * (heightInBlocks -1));
         this.position =[newX, newY];
       };
       this.isOnSnake = (snakeToCheck) =>{ // METHOD QUI PERMET D'ÉVITER QUE LE POMME APPARAISSE SUR LE SERPENT
 
-        let isOnSnake = false;
+        const isOnSnake = false;
         for(let i in snakeToCheck.body) // LA BOUCLE PASSE SUR TOUTE LA LONGUEUR DU CORPS DU SERPENT
           {
             if (this.position[0] === snakeToCheck.body[i][0] && this.position[1] === snakeToCheck.body[i][1]){ // ON FÉRIFIE QUE LA POMME NE SE TROUVE PAS SUR LE X OU SUR LE Y
@@ -226,7 +227,7 @@ window.onload = function()
     }
 
     document.onkeydown = function handleKeyDown(e){  //Method couplé d'un évènement pour diriger le serpent
-      let key = e.keyCode;
+      const key = e.keyCode;
       let newDirection;
       switch(key){
         case 37 :
